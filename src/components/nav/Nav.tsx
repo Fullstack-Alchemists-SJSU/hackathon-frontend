7064c22ffa1b55ed7db033c3076e621e85c258a3
ShivamHasurkarSJSU <shivamravindra.hasurkar@sjsu.edu>
2023-11-19 12:42:00 -0800

    Title change

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -2,7 +2,7 @@ import "./nav.css"
 const Nav = () => {
     return (
         <div className="nav-container">
-            <div className="nav-title">MacroEconomic Reseach Chat Assistant</div>
+            <div className="nav-title">MacroEconomic Research Chat Assistant</div>
         </div>
     )
 }
